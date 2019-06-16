81aa5829bfdc59c4f854ad12c11ef4ebc0abe5a2
Tom Westerhout <t.westerhout@student.ru.nl>
2019-06-15 23:09:04 -0400

    Move check_license.sh script to Test/; Start verifying headers on Travis

--- Test/check_license.sh
+++ Test/check_license.sh
@@ -1,16 +1,40 @@
+# Copyright 2019 The Simons Foundation, Inc. - All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #!/bin/bash
 
 _get_year_specs() {
+	declare -A exceptions
+	# We do our best to guess the years correctly, but sometimes, git log
+	# --follow gets stuff wrong.  If you're absolutely sure that you've
+	# encountered such an example, add it to the exception list below
+	exceptions["netket/__init__.py"]="2019"
+
 	if [ $# -ne 1 ] ; then echo "expected one argument: filename"; return 1; fi
-	years=$(git blame -p "$1" | grep -E "^author-time" | cut -d' ' -f 2 | sort -n)
-	if [ -z "$years" ] ; then
-		date +%Y
-		return
-	fi
-	min_year=$(echo "$years" | head -n 1)
+	if [ -n "${exceptions[$1]}" ] ; then echo ${exceptions["$1"]}; return; fi
+	years_log=$(git log --follow --pretty=%ad --date=short "$1" | cut -d'-' -f 1 | sort -n)
+	years_blame=$(git blame -p "$1" | grep -E "^author-time" | cut -d' ' -f 2 | sort -n)
+	if [ -z "$years_log" ] && [ -z "$years_blame" ] ; then date +%Y; return; fi
+	min_year=$(echo "$years_blame" | head -n 1)
 	min_year=$(date +%Y -d @$min_year)
-	max_year=$(echo "$years" | tail -n 1)
+	if [ -n "$years_log" ] && [ $(echo "$years_log" | head -n 1) -lt $min_year ] ; then
+		min_year=$(echo "$years_log" | head -n 1)
+	fi
+	max_year=$(echo "$years_blame" | tail -n 1)
 	max_year=$(date +%Y -d @$max_year)
+	# if [ -n "$years_log" ] && [ $(echo "$years_log" | tail -n 1) -gt $max_year ] ; then
+	# 	max_year=$(echo "$years_log" | tail -n 1)
+	# fi
 	if [ $min_year -ne $max_year ] ; then
 		echo "$min_year-$max_year"
 	else
@@ -50,18 +74,23 @@ _check_copyright() {
 	if [ -n "$output" ] ; then
 		echo -e "\e[1;37m$filename:\e[0m"
 		echo -e "$output" | sed 's/^/    /'
+		return 1
 	fi
 }
 
+STATUS=0
 for f in $(git ls-files); do
 	case "$f" in
 		*CMakeLists.txt) ;&
 		*check_license.sh) ;&
 		Examples/*.py|Examples/*.cc|Examples/*.hpp) ;&		
-		Sourses/*.py|Sources/*.cc|Sources/*.hpp) ;&		
+		# Sourses/*.py|Sources/*.cc|Sources/*.hpp) ;&		
 		Test/*.py|Test/*.cc|Test/*.hpp) ;&		
 		setup.py|netket/*.py|netket/*.cc|netket/*.hpp)
 			_check_copyright "$f"
+			_status=$?
+			[ $_status -ne 0 ] && STATUS=$_status
 		;;
 	esac
 done
+exit $STATUS
