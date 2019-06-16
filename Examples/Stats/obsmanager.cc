7bfa31d3c2d3dc38091b3f0eb71f067c4089ff53
Tom Westerhout <t.westerhout@student.ru.nl>
2019-06-15 22:11:33 -0400

    Fix license headers in CMakeLists.txt and Examples/

--- Examples/Stats/obsmanager.cc
+++ Examples/Stats/obsmanager.cc
@@ -1,77 +1,86 @@
+// Copyright 2018-2019 The Simons Foundation, Inc. - All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cassert>
 #include <iostream>
 #include <random>
 #include <vector>
-#include <random>
-#include <cassert>
 #include "netket.hpp"
 
 using namespace std;
 using namespace netket;
 
-double loggaussian(double x){
-  return -(x*x);
-}
+double loggaussian(double x) { return -(x * x); }
 
-double GaussianWalk(double x,std::mt19937 & gen,double step_size){
+double GaussianWalk(double x, std::mt19937& gen, double step_size) {
   std::uniform_real_distribution<> dis(0, 1.0);
 
-  double xnew=x+(dis(gen)-0.5)*step_size;
+  double xnew = x + (dis(gen) - 0.5) * step_size;
 
-  if(std::exp(loggaussian(xnew)-loggaussian(x))>dis(gen)){
+  if (std::exp(loggaussian(xnew) - loggaussian(x)) > dis(gen)) {
     return xnew;
-  }
-  else{
+  } else {
     return x;
   }
 }
 
-int main(int argc,char * argv[]){
-  MPI_Init(&argc,&argv);
+int main(int argc, char* argv[]) {
+  MPI_Init(&argc, &argv);
 
-  std::random_device rd;  //Will be used to obtain a seed for the random number engine
-  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::random_device
+      rd;  // Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd());  // Standard mersenne_twister_engine seeded with rd()
 
-  double x=0;
+  double x = 0;
 
   ObsManager obm;
 
-  obm.AddObservable("GaussianWalk1",double());
+  obm.AddObservable("GaussianWalk1", double());
 
-  for(int i=0;i<100000;i++){
-    x=GaussianWalk(x,gen,1);
-    obm.Push("GaussianWalk1",x);
+  for (int i = 0; i < 100000; i++) {
+    x = GaussianWalk(x, gen, 1);
+    obm.Push("GaussianWalk1", x);
   }
 
-  obm.AddObservable("GaussianWalk2",double());
-  for(int i=0;i<100000;i++){
-    x=GaussianWalk(x,gen,2);
-    obm.Push("GaussianWalk2",x);
+  obm.AddObservable("GaussianWalk2", double());
+  for (int i = 0; i < 100000; i++) {
+    x = GaussianWalk(x, gen, 2);
+    obm.Push("GaussianWalk2", x);
   }
 
-  obm.AddObservable("VectorObs",VectorXd());
+  obm.AddObservable("VectorObs", VectorXd());
   VectorXd gw(2);
   gw.setZero();
 
-  for(int i=0;i<100000;i++){
-    for(int k=0;k<2;k++){
-      gw(k)=GaussianWalk(gw(k),gen,k+1);
+  for (int i = 0; i < 100000; i++) {
+    for (int k = 0; k < 2; k++) {
+      gw(k) = GaussianWalk(gw(k), gen, k + 1);
     }
-    obm.Push("VectorObs",gw);
+    obm.Push("VectorObs", gw);
   }
-
 
   // cout<<json(obm)<<endl;
 
   json j;
-  auto jout=json(obm);
-  jout["Iteration"]=1;
+  auto jout = json(obm);
+  jout["Iteration"] = 1;
   j["Output"].push_back(jout);
 
-  jout=json(obm);
-  jout["Iteration"]=2;
+  jout = json(obm);
+  jout["Iteration"] = 2;
   j["Output"].push_back(jout);
-  cout<<j<<endl;
+  cout << j << endl;
 
   MPI_Finalize();
-
 }
--- Examples/Stats/vector_stats.cc
+++ Examples/Stats/vector_stats.cc
@@ -1,67 +1,78 @@
+// Copyright 2018-2019 The Simons Foundation, Inc. - All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <cassert>
 #include <iostream>
 #include <random>
-#include <vector>
-#include <random>
-#include <cassert>
 #include <valarray>
+#include <vector>
 
 #include "netket.hpp"
 
 using namespace std;
 using namespace netket;
 
-double loggaussian(double x,double mean){
-  return -((x-mean)*(x-mean));
-}
+double loggaussian(double x, double mean) { return -((x - mean) * (x - mean)); }
 
-double GaussianWalk(double x,std::mt19937 & gen,double mean,double step_size){
+double GaussianWalk(double x, std::mt19937& gen, double mean,
+                    double step_size) {
   std::uniform_real_distribution<> dis(0, 1.0);
 
-  double xnew=x+(dis(gen)-0.5)*step_size;
+  double xnew = x + (dis(gen) - 0.5) * step_size;
 
-  if(std::exp(loggaussian(xnew,mean)-loggaussian(x,mean))>dis(gen)){
+  if (std::exp(loggaussian(xnew, mean) - loggaussian(x, mean)) > dis(gen)) {
     return xnew;
-  }
-  else{
+  } else {
     return x;
   }
 }
 
-int main(int argc,char * argv[]){
-  MPI_Init(&argc,&argv);
+int main(int argc, char* argv[]) {
+  MPI_Init(&argc, &argv);
 
-  std::random_device rd;  //Will be used to obtain a seed for the random number engine
-  std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+  std::random_device
+      rd;  // Will be used to obtain a seed for the random number engine
+  std::mt19937 gen(rd());  // Standard mersenne_twister_engine seeded with rd()
 
   Binning<VectorXd> binning(16);
 
   VectorXd x(10);
   x.setZero();
 
-  for(int i=0;i<100000;i++){
-    for(int j=0;j<10;j++){
-      x(j)=GaussianWalk(x(j),gen,j,0.2*(1+j));
+  for (int i = 0; i < 100000; i++) {
+    for (int j = 0; j < 10; j++) {
+      x(j) = GaussianWalk(x(j), gen, j, 0.2 * (1 + j));
     }
-    binning<<x;
+    binning << x;
   }
 
   int rank;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  auto taucorr=binning.TauCorr();
-  auto mean=binning.Mean();
-  auto eom=binning.ErrorOfMean();
-  int N=binning.N();
+  auto taucorr = binning.TauCorr();
+  auto mean = binning.Mean();
+  auto eom = binning.ErrorOfMean();
+  int N = binning.N();
 
-  if(rank==0){
-    cout<<N<<" "<<endl;
-    for(int i=0;i<mean.size();i++){
-      cout<<mean(i)<<"  "<<eom(i)<<"  "<<taucorr(i)<<endl;
+  if (rank == 0) {
+    cout << N << " " << endl;
+    for (int i = 0; i < mean.size(); i++) {
+      cout << mean(i) << "  " << eom(i) << "  " << taucorr(i) << endl;
     }
   }
 
-  json j=binning.AllStats();
-  cout<<j<<endl;
+  json j = binning.AllStats();
+  cout << j << endl;
   MPI_Finalize();
-
 }
